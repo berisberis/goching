e5b8cc1d104a7afb7366580c1192d2dbfebf23b5
Jonatan Beristain <berisberis@gmail.com>
2023-04-13 21:25:56 -0700

    simplified codebase

--- pkg/hexgen/hexagram.go
+++ pkg/hexgen/hexagram.go
@@ -1,19 +1,36 @@
 package hexgen
 
+import (
+	"crypto/rand"
+)
+
+type Trigram struct {
+	FirstLine  int
+	SecondLine int
+	ThirdLine  int
+}
+
 type Hexagram struct {
-	top    int64
-	bottom int64
+	UpperTrigram Trigram
+	LowerTrigram Trigram
 }
 
-func (h Hexagram) Generate() []int64 {
-	hex := make([]int64, 0, 2)
-	hex = append(hex, h.top, h.bottom)
-	return hex
-}
-
-func NewHexagram(top, bottom int64) *Hexagram {
-	return &Hexagram{
-		top:    top,
-		bottom: bottom,
+func TossCoin() int {
+	b := make([]byte, 1)
+	rand.Read(b)
+	if b[0]%2 == 0 {
+		return 1 // Heads
+	} else {
+		return 0 // Tails
 	}
+}
+
+func GenerateTrigram() Trigram {
+	return Trigram{TossCoin(), TossCoin(), TossCoin()}
+}
+
+func GenerateHexagram() Hexagram {
+	trigram1 := GenerateTrigram()
+	trigram2 := GenerateTrigram()
+	return Hexagram{trigram1, trigram2}
 }
